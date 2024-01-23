1b298c8cc4b8b72502ab03e35bca083d8255eb50
Matheus <math.leal14@gmail.com>
2024-01-22 22:30:27 -0300

    numero secreto alterado para 1 a 100

--- js-curso-2-aula1/js-curso-2-aula_-/app.js
+++ js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -5,7 +5,7 @@ titulo.innerHTML = 'Jogo do número secreto'; */
 paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 */
 let listaDeNumerosSorteados = [];
-let numeroLimite = 50;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -15,7 +15,7 @@ function exibirTextoNaTela(tag, texto){
 }
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 50');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
 
 exibirMensagemInicial();
